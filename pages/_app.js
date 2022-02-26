b1d185315e4aca9765905e973cba4711ca2169bf
Patrick Sparks <pto1026@gmail.com>
2022-02-26 01:29:40 -0700

    bootstrap added, using _document.js to modify the head to include the css cdn link and in _app.js using the bundled js bootstrap cdn link at the end of the body and lazy loading it.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+        <Component {...pageProps} />
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                crossOrigin="anonymous"
+                strategy='lazyOnload'></Script>
+      </>
+  )
 }
 
 export default MyApp
